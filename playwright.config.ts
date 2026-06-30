301fad17a1326727de20afb1094f709ae17e0ccf
duongphanhoai <pollyfixx@gmail.com>
2026-06-30 11:36:06 +0700

    create failed to fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ if (!process.env.__PW_BANNER_PRINTED) {
 
 export default defineConfig({
   testDir: "tests",
-  timeout: 30_000,
+  timeout: 10_000,
   retries: 0,
   workers: process.env.CI ? 2 : 4,
   reporter: [
